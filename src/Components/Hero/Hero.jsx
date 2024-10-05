c4c293abdde1405c049cd3b79474578d3c6f0ee2
HasanC14 <chowdhuryh999@gmail.com>
2024-10-06 03:28:57 +0600

    hero section done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import large from "/images/1600x400.png";
+import medium from "/images/43.png";
+import small from "/images/84.png";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="relative">
+      <picture className="w-full">
+        <source media="(max-width: 500px)" srcSet={small} className="w-full" />
+        <source media="(max-width: 800px)" srcSet={medium} className="w-full" />
+        <img src={large} className="w-full" alt="Hero" />
+      </picture>
+      <button className="absolute hover:bg-red-600 top-1/2 right-1/4 bg-prime text-white py-3 md:px-5 font-bold lg:px-10 px-2 md:text-lg text-xs rounded-md uppercase">
+        Order now
+      </button>
+    </div>
+  );
 };
 
 export default Hero;
